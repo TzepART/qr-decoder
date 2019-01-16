b054e539ae87ec9b2af32d2081d113eff5f40205
tzepart <tzepart@gmail.com>
2019-01-16 19:13:08 +0300

    w_fix

--- web/index.php
+++ web/index.php
@@ -9,6 +9,12 @@
 require __DIR__ . "/../vendor/autoload.php";
 //ini_set('memory_limit','-1');
 
-$fileHelper = new \App\Helper\FileHelper("/Users/artem/PhpstormProjects/qr_decoder/web/good_data/");
-$results = (new \App\AppQrReader($fileHelper))->getResult();
+use \App\Helper\FileHelper;
+use App\AppQrReader;
+
+
+$dataDir = "/Users/artem/PhpstormProjects/qr_decoder/web/good_data/";
+
+$fileHelper = new FileHelper($dataDir);
+$results = (new AppQrReader($fileHelper))->getResult();
 var_dump($results);
