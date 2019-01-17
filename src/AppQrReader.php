961ef7b4ab6239cba95f6a2d9a244c32433cc86e
tzepart <tzepart@gmail.com>
2019-01-17 17:38:14 +0300

    w_add_using_resource_instead_files

--- src/AppQrReader.php
+++ src/AppQrReader.php
@@ -33,26 +33,42 @@ class AppQrReader
     }
 
     /**
-     * @param bool $blackWhite
+     * @param bool $optimizeSize
      * @return array
      */
-    public function getResult($blackWhite = true)
+    public function getResult($optimizeSize = true)
     {
         $results = [];
         foreach ($this->fileHelper->getFiles() as $file) {
             $path = $this->fileHelper->getWorkDir().$file;
+            //create resource
+            $img = $this->getFileResource($path);
+
             try {
-                if($blackWhite){
-                    $path = $this->fileHelper->toBlackAndWhite($file);
+                if($optimizeSize){
+                    $img = $this->fileHelper->optimizeSize($img);
                 }
-                $qrcode = new QrReader($path);
+                $qrcode = new QrReader($img, QrReader::SOURCE_TYPE_RESOURCE, false);
                 $results[$path] = $qrcode->text(); //return decoded text from QR Code
             } catch (\Exception $exception) {
                 $results[$path] = $exception->getMessage();
+                imagedestroy($img);
             }
-//            echo memory_get_usage() . PHP_EOL;
+
+            echo memory_get_usage() . PHP_EOL;
         }
 
         return $results;
+    }
+
+    /**
+     * @param $path
+     * @return resource
+     */
+    protected function getFileResource(string $path)
+    {
+        //TODO make validation and choose type
+        $img = imagecreatefromjpeg($path);
+        return $img;
     }
 }
